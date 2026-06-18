667a637ab3b3236bfefb68c311a11774e1e10956
Danilo Silva <dsilva68@uolinc.com>
2026-06-18 16:46:23 -0300

    feat(SAR-2228): Adicionado funcionalidade de cookie e uso real do cookie

---
+++ cookie_consent/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CookieConsent from "./components/CookieConsent";
+import Conteudo from "./components/Conteudo";
+import Saudacao from "./components/Saudacao";
+
+function App() {
+	const [abaSelecionada, setAbaSelecionada] = useState("primeira");
+	return (
+		<>
+			<main className="App">
+				<Box
+					sx={{
+						flexDirection: "row",
+						display: "flex",
+						justifyContent: "flex-start",
+						gap: 2,
+						marginTop: 2,
+						marginBottom: 2
+					}}
+				>
+					<Button
+						sx={{
+							color: abaSelecionada === "primeira" ? "blue" : "text.primary",
+						}}
+						variant="text"
+						size="medium"
+						onClick={() => setAbaSelecionada("primeira")}
+					>
+						First Tab
+					</Button>
+					<Button
+						sx={{
+							color: abaSelecionada  === "segunda" ? "blue" : "text.primary",
+						}}
+						variant="text"
+						size="medium"
+						onClick={() => setAbaSelecionada("segunda")}
+					>
+						Second Tab
+					</Button>
+					<Button
+						sx={{
+							color: abaSelecionada  === "terceira" ? "blue" : "text.primary",
+						}}
+						variant="text"
+						size="medium"
+						onClick={() => setAbaSelecionada("terceira")}
+					>
+						Third Tab
+					</Button>
+					<Button
+						sx={{
+							color: abaSelecionada  === "quarta" ? "blue" : "text.primary",
+						}}
+						variant="text"
+						size="medium"
+						onClick={() => setAbaSelecionada("quarta")}
+					>
+						Fourth Tab
+					</Button>
+				</Box>
+				<p className="how-use">Clique em cada botão para ser mostrado o seu respectivo conteúdo.</p>
+				<section className="tab-content">
+					<Conteudo abaAtiva={abaSelecionada}/>
+				</section>
+				<Saudacao/>
+				<CookieConsent />
+			</main>
+			<footer>&copy; Licenciado sob MIT</footer>
+		</>
+	);
+}
+export default App;
