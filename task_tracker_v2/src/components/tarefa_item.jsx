c152d227ee48a98164795368f4b922d0badcb090
Danilo Silva <dsilva68@uolinc.com>
2026-07-07 17:51:02 -0300

    refactor(SAR-2230): substiduido prompt por DIalog na edição das tarefas

--- task_tracker_v2/src/components/tarefa_item.jsx
+++ task_tracker_v2/src/components/tarefa_item.jsx
@@ -3,12 +3,19 @@ import {
 	ListItemText,
 	Divider,
 	IconButton,
+	Button,
 	Box,
 	Checkbox,
 	ListItemIcon,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	TextField,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
 
 export default function TarefaItem({
 	tarefa,
@@ -17,6 +24,20 @@ export default function TarefaItem({
 	onAlternarStatus,
 }) {
 	const isConcluida = tarefa.status === "concluida";
+	const [abrirDialog, setAbrirDialog] = useState(false);
+	const [novoNome, setNovoNome] = useState(tarefa.nome);
+
+	const salvarEdicao = () => {
+		if (novoNome.trim() !== "") {
+			onEditar(tarefa.id, novoNome);
+			setAbrirDialog(false);
+		}
+	};
+
+	const fecharDialog = () => {
+		setNovoNome(tarefa.nome);
+		setAbrirDialog(false);
+	};
 
 	return (
 		<ListItem
@@ -27,15 +48,38 @@ export default function TarefaItem({
 						aria-label="editar tarefa"
 						sx={{ mr: 1 }}
 						onClick={() => {
-							const novoNome = prompt(
-								"digite o novo nome da Tarefa:",
-								tarefa.nome,
-							);
-							if (novoNome) onEditar(tarefa.id, novoNome);
+							setAbrirDialog(true);
 						}}
 					>
 						<EditIcon color="primary" />
 					</IconButton>
+
+					<Dialog open={abrirDialog} onClose={fecharDialog}>
+						<DialogTitle>Editar Tarefa</DialogTitle>
+						<DialogContent>
+							<TextField
+								autoFocus
+								margin="dense"
+								label="Nome da Tarefa"
+								type="text"
+								fullWidth
+								variant="outlined"
+								value={novoNome}
+								onChange={(evento) => setNovoNome(evento.target.value)}
+                onKeyPress={(evento) => (
+                  evento.key === "Enter" ? salvarEdicao() : null
+                )}
+							/>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={fecharDialog} color="primary">
+								Cancelar
+							</Button>
+							<Button onClick={salvarEdicao} color="primary">
+								Salvar
+							</Button>
+						</DialogActions>
+					</Dialog>
 
 					<IconButton
 						edge="end"
